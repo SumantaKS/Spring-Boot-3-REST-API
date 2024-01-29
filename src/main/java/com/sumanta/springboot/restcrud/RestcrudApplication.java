ea0c9168ac61436170b8ce5a47934502029ccbfe
Sumanta Singh <2758836s@student.gla.ac.uk>
2024-01-29 22:46:10 +0000

    Maven initialiser files for the project

---
+++ src/main/java/com/sumanta/springboot/restcrud/RestcrudApplication.java
@@ -0,0 +1,13 @@
+package com.sumanta.springboot.restcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumanta/springboot/restcrud/RestcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumanta.springboot.restcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
