2b6fbefc053ac95f9172bea6b61197ea46f8722b
Sumanta Singh <2758836s@student.gla.ac.uk>
2024-02-01 03:45:37 +0000

    added SQL scripts

---
+++ sql script/security.sql
@@ -0,0 +1,95 @@
+USE `employee_directory`;
+
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `role`;
+SET foreign_key_checks = 1;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` char(80) NOT NULL,
+  `enabled` tinyint NOT NULL,  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+--
+
+--
+-- passwords here are: parker, jane and osborn respectively
+--
+
+INSERT INTO `user` (`username`,`password`,`enabled`)
+VALUES 
+('peter','$2a$10$CDS9AvXvbLnKky4MO3KmgeZGdBSXsZQU6QxKqUqfMj2Os.NC2sA2C',1),
+('mary','$2a$10$8N00SjMPB58fOUkZGtecKuJZnpUirqN2rODK7S1vMU5MrEcCsnAYW',1),
+('harry','$2a$10$DAGpObGwTq1TO/ULTffeJ.n5OAHZ/rNIH5LvOMOkeQlGqHPSCzpFO',1);
+
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `role` (name)
+VALUES 
+('ROLE_EMPLOYEE'),('ROLE_MANAGER'),('ROLE_ADMIN');
+
+--
+-- Table structure for table `users_roles`
+--
+
+DROP TABLE IF EXISTS `users_roles`;
+
+CREATE TABLE `users_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  
+  PRIMARY KEY (`user_id`,`role_id`),
+  
+  KEY `FK_ROLE_idx` (`role_id`),
+  
+  CONSTRAINT `FK_USER_05` FOREIGN KEY (`user_id`) 
+  REFERENCES `user` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+  
+  CONSTRAINT `FK_ROLE` FOREIGN KEY (`role_id`) 
+  REFERENCES `role` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+--
+-- Dumping data for table `users_roles`
+--
+
+INSERT INTO `users_roles` (user_id,role_id)
+VALUES 
+(1, 1),
+(2, 1),
+(2, 2),
+(3, 1),
+(3, 2),
+(3, 3)
